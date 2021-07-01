b8db1e9d8fbe5736c2072344c4eb18145b366124
sandy <sandytang2406@gmail.com>
2021-07-01 20:46:32 +0200

    create Course Class #2

--- prototypes/online-dance-practice-backend/src/main/java/at/htl/entity/Course.java
+++ prototypes/online-dance-practice-backend/src/main/java/at/htl/entity/Course.java
@@ -1,4 +1,43 @@
 package at.htl.entity;
 
-public class Course {
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "D_COURSE")
+public class Course extends PanacheEntityBase {
+
+    @Id
+    public String type;
+
+    @Enumerated(EnumType.ORDINAL)
+    Content content;
+
+    @Enumerated(EnumType.ORDINAL)
+    Level level;
+
+    public Course(String type, Content content, Level level) {
+        this.type = type;
+        this.content = content;
+        this.level = level;
+    }
+
+    public Course() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(type, course.type) && content == course.content && level == course.level;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, content, level);
+    }
 }
