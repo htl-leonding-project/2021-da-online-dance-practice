6b869a4ddc9273159f6462e3c4d417cfaa790f63
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2021-07-05 17:33:53 +0200

    implemented InitBean and test data for db #6

---
+++ prototypes/online-dance-practice-backend/src/main/java/at/htl/control/InitBean.java
@@ -0,0 +1,46 @@
+package at.htl.control;
+
+import at.htl.entity.Content;
+import at.htl.entity.Course;
+import at.htl.entity.Level;
+import at.htl.entity.User;
+import io.quarkus.runtime.StartupEvent;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+public class InitBean {
+
+    @Inject
+    CourseRepository courseRepository;
+
+    @Inject
+    UserRepository userRepository;
+
+    @Transactional
+    void onStart(@Observes StartupEvent event) {
+        initDb();
+    }
+
+    private void initDb() {
+
+        User samuel = new User();
+        userRepository.persist(samuel);
+        User anna = new User();
+        userRepository.persist(anna);
+        User max = new User();
+        userRepository.persist(max);
+        User julia = new User();
+        userRepository.persist(julia);
+
+
+        Course latein = new Course("Langsamer Walzer", Content.valueOf("VIDEO"), Level.valueOf("GRUNDKURS"));
+        courseRepository.persist(latein);
+        Course samba = new Course("Slow Fox", Content.valueOf("AUDIO"), Level.valueOf("BRONZE"));
+        courseRepository.persist(samba);
+        Course rumba = new Course("Tango", Content.valueOf("AUDIO"), Level.valueOf("SILBER"));
+        courseRepository.persist(rumba);
+    }
+
+}
