85456bf2c97c0a7d62c0a317e393206dc10fbf7a
rosaliemandel <r.mandel@students.htl-leonding.ac.at>
2021-07-05 18:06:59 +0200

    add video and audio components #11

---
+++ prototypes/online-dance-practice-backend/src/main/frontend/src/app/audio-input/audio-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-input',
+  templateUrl: './audio-input.component.html',
+  styleUrls: ['./audio-input.component.css']
+})
+export class AudioInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ prototypes/online-dance-practice-backend/src/main/frontend/src/app/video-input/video-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-input',
+  templateUrl: './video-input.component.html',
+  styleUrls: ['./video-input.component.css']
+})
+export class VideoInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
