86c9e3014fb37d512f489b6895e4b2eff03260f7
sandy <sandytang2406@gmail.com>
2021-07-22 12:26:40 +0200

    add Columnname #2

---
+++ prototypes/online-dance-practice-backend/src/main/java/at/htl/entity/Booking.java
@@ -0,0 +1,33 @@
+package at.htl.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "D_BOOKING")
+public class Booking extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "B_BOOKINGID")
+    public Long bookingId;
+
+    @ManyToOne
+    @JoinColumn(name = "B_USERID")
+    public User user;
+
+    @ManyToOne
+    @JoinColumn(name = "B_COURSEID")
+    public Course course;
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "bookingId=" + bookingId +
+                ", user=" + user +
+                ", course=" + course +
+                '}';
+    }
+
+}
