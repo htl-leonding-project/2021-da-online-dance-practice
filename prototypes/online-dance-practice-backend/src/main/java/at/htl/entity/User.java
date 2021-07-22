56b3a71ae10e7cbfa174adfc297a1f612542743b
sandy <sandytang2406@gmail.com>
2021-07-22 12:17:49 +0200

    adjust user #2

--- prototypes/online-dance-practice-backend/src/main/java/at/htl/entity/User.java
+++ prototypes/online-dance-practice-backend/src/main/java/at/htl/entity/User.java
@@ -3,7 +3,6 @@ package at.htl.entity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
 @Table(name = "D_USER")
@@ -11,10 +10,25 @@ public class User extends PanacheEntityBase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
+    public Long userId;
+
+    String username;
+
+    String firstname;
+
+    String lastname;
 
     public User() {
     }
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
 
 }
