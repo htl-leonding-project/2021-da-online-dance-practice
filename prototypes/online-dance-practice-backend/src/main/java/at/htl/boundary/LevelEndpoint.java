3f742b939ddd5129b166466bba8423f69b5db456
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2021-07-26 12:29:11 +0200

    add create to LevelEnpoint #15

--- prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/LevelEndpoint.java
+++ prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/LevelEndpoint.java
@@ -1,14 +1,17 @@
 package at.htl.boundary;
 
 import at.htl.control.LevelRepository;
+import at.htl.entity.Level;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 
 @RequestScoped
 @Path("/Level")
@@ -25,6 +28,15 @@ public class LevelEndpoint {
     }
 
 
+    @POST
+    @Path("/create")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Level level, @Context UriInfo info) {
+        levelRepository.persist(level);
+        return Response.created(URI.create(info.getPath() + "/"+ level.levelId)).build();
+    }
 
 
 
