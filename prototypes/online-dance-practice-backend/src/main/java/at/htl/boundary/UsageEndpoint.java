c79d7f4967dd6e344199606abb0fec2e6066bfa5
sandy <sandytang2406@gmail.com>
2021-07-26 12:32:09 +0200

    add UsageEndpoint create #15

--- prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/UsageEndpoint.java
+++ prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/UsageEndpoint.java
@@ -1,14 +1,17 @@
 package at.htl.boundary;
 
 import at.htl.control.UsageRepository;
+import at.htl.entity.Usage;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 
 @RequestScoped
 @Path("/Usage")
@@ -22,6 +25,16 @@ public class UsageEndpoint {
     @Produces(MediaType.APPLICATION_JSON)
     public Response findAll() {
         return Response.ok(usageRepository.findAll()).build();
+    }
+
+    @POST
+    @Path("/create")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Usage usage, @Context UriInfo info) {
+        usageRepository.persist(usage);
+        return Response.created(URI.create(info.getPath() + "/"+ usage.usageId)).build();
     }
 
 }
