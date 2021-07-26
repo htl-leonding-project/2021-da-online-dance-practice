1c16d4686598babb2b4afd3a8f656b2736e3fe7f
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2021-07-26 18:20:47 +0200

    simple routing in app.component.html and content.component.html  #11

--- prototypes/online-dance-practice-backend/src/main/frontend/src/app/app.module.ts
+++ prototypes/online-dance-practice-backend/src/main/frontend/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AudioInputComponent } from './audio-input/audio-input.component';
-import { VideoInputComponent } from './video-input/video-input.component';
+import { AudioInputComponent } from './content/audio-input/audio-input.component';
+import { VideoInputComponent } from './content/video-input/video-input.component';
 import { DanceLevelComponent } from './dance-level/dance-level.component';
 import { DanceCourseComponent } from './dance-course/dance-course.component';
 import { ContentComponent } from './content/content.component';
