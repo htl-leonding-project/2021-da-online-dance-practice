b3e5f9c76d8ca60121fbf1e3faadf5edfae975ab
sandy <sandytang2406@gmail.com>
2021-07-26 12:35:29 +0200

    fix userRepository #15

--- prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/UserEndpoint.java
+++ prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/UserEndpoint.java
@@ -1,14 +1,22 @@
 package at.htl.boundary;
 
+import at.htl.control.UserRepository;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @RequestScoped
 @Path("/consumation")
 public class UserEndpoint {
 
     @Inject
-    UserEndpoint userEndpoint;
+    UserRepository userRepository;
+
+
 
 }
