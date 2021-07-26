48edea48c7a91ee43b7bbfe568d54f3d2ebdc182
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2021-07-26 12:22:19 +0200

    inject levelrepository #15

--- prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/LevelEndpoint.java
+++ prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/LevelEndpoint.java
@@ -4,7 +4,11 @@ import at.htl.control.LevelRepository;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @RequestScoped
 @Path("/Level")
@@ -12,4 +16,8 @@ public class LevelEndpoint {
 
     @Inject
     LevelRepository levelRepository;
+
+
+
+
 }
