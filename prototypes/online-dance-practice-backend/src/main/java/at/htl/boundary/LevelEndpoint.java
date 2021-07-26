b68471b9edab2997bc855292b9727209e7836dd5
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2021-07-26 12:23:15 +0200

    add findAll to LevelEnpoint #15

--- prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/LevelEndpoint.java
+++ prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/LevelEndpoint.java
@@ -17,6 +17,14 @@ public class LevelEndpoint {
     @Inject
     LevelRepository levelRepository;
 
+    @GET
+    @Path("/findAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findAll() {
+        return Response.ok(levelRepository.findAll()).build();
+    }
+
+
 
 
 
