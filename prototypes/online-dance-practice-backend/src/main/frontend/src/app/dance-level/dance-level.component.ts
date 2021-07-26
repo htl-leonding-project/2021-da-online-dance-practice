9c6fbe73ccda0bea5eb26b140702f1f1e5f77c81
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2021-07-26 18:03:00 +0200

    create danceLevel component and danceCourse component #11

---
+++ prototypes/online-dance-practice-backend/src/main/frontend/src/app/dance-level/dance-level.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dance-level',
+  templateUrl: './dance-level.component.html',
+  styleUrls: ['./dance-level.component.css']
+})
+export class DanceLevelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
