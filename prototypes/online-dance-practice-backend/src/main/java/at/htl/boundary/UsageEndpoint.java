ac3a0d9b62e37efba667dd186b23909376536e5c
sandy <sandytang2406@gmail.com>
2021-07-26 12:29:52 +0200

    add UsageEndpoint findAll #15

--- prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/UsageEndpoint.java
+++ prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/UsageEndpoint.java
@@ -4,7 +4,11 @@ import at.htl.control.UsageRepository;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @RequestScoped
 @Path("/Usage")
@@ -12,5 +16,12 @@ public class UsageEndpoint {
 
     @Inject
     UsageRepository usageRepository;
+
+    @GET
+    @Path("/findAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findAll() {
+        return Response.ok(usageRepository.findAll()).build();
+    }
 
 }
