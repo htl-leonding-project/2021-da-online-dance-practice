4294767f575ccfcab8b318ed1291fac9d49f99f6
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2021-07-26 12:39:13 +0200

    add create to LevelEnpoint #15

--- prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/BookingEnpoint.java
+++ prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/BookingEnpoint.java
@@ -1,14 +1,17 @@
 package at.htl.boundary;
 
 import at.htl.control.BookingRepository;
+import at.htl.entity.Booking;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 
 @RequestScoped
 @Path("/")
@@ -22,6 +25,17 @@ public class BookingEnpoint {
     @Produces(MediaType.APPLICATION_JSON)
     public Response findAll() {
         return Response.ok(bookingRepository.findAll()).build();
+    }
+
+
+    @POST
+    @Path("/create")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Booking booking, @Context UriInfo info) {
+        bookingRepository.persist(booking);
+        return Response.created(URI.create(info.getPath() + "/"+ booking.bookingId)).build();
     }
 
 }
