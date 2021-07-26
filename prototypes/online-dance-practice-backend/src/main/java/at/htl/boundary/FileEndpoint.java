48ca5c792a507b06aac00a5ff01e0cb868508723
sandy <sandytang2406@gmail.com>
2021-07-26 12:25:35 +0200

    add create #15

--- prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/FileEndpoint.java
+++ prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/FileEndpoint.java
@@ -2,14 +2,17 @@ package at.htl.boundary;
 
 import at.htl.control.CourseRepository;
 import at.htl.control.FileRepository;
+import at.htl.entity.File;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 
 @RequestScoped
 @Path("/File")
@@ -25,4 +28,13 @@ public class FileEndpoint {
         return Response.ok(fileRepository.findAll()).build();
     }
 
+    @POST
+    @Path("/create")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(File file, @Context UriInfo info) {
+        fileRepository.persist(file);
+        return Response.created(URI.create(info.getPath() + "/"+ file.fileId)).build();
+    }
 }
