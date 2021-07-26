fb9abf2c714de25d2cd09173096d7ecb56eb3bf5
sandy <sandytang2406@gmail.com>
2021-07-26 12:36:52 +0200

    add UserEndpoint create #15

--- prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/UserEndpoint.java
+++ prototypes/online-dance-practice-backend/src/main/java/at/htl/boundary/UserEndpoint.java
@@ -1,14 +1,17 @@
 package at.htl.boundary;
 
 import at.htl.control.UserRepository;
+import at.htl.entity.User;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 
 @RequestScoped
 @Path("/consumation")
@@ -24,5 +27,14 @@ public class UserEndpoint {
         return Response.ok(userRepository.findAll()).build();
     }
 
+    @POST
+    @Path("/create")
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(User user, @Context UriInfo info) {
+        userRepository.persist(user);
+        return Response.created(URI.create(info.getPath() + "/"+ user.userId)).build();
+    }
 
 }
