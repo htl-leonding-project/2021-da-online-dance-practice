de441531162f99446c3c74dfab5eabd28dd3430c
sandy <sandytang2406@gmail.com>
2021-08-16 20:38:42 +0200

    fixed paths #15

--- backend/src/main/java/at/htl/boundary/LevelEndpoint.java
+++ backend/src/main/java/at/htl/boundary/LevelEndpoint.java
@@ -14,14 +14,14 @@ import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 
 @RequestScoped
-@Path("/Level")
+@Path("/level")
 public class LevelEndpoint {
 
     @Inject
     LevelRepository levelRepository;
 
     @GET
-    @Path("/findAll")
+    @Path("/findall")
     @Produces(MediaType.APPLICATION_JSON)
     public Response findAll() {
         return Response.ok(levelRepository.findAll()).build();
@@ -65,7 +65,5 @@ public class LevelEndpoint {
                     .build();
         }
     }
-
-
 
 }
