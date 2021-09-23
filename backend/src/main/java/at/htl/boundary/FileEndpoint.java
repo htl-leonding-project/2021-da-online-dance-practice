59425426f988aac61280ea3b8538e82121349421
sandy <sandytang2406@gmail.com>
2021-09-23 09:47:07 +0200

    file upload #danc-10

--- backend/src/main/java/at/htl/boundary/FileEndpoint.java
+++ backend/src/main/java/at/htl/boundary/FileEndpoint.java
@@ -6,18 +6,24 @@ import at.htl.entity.ContentType;
 import at.htl.entity.Course;
 import at.htl.entity.D_File;
 import at.htl.entity.Level;
+import org.apache.commons.io.IOUtils;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.servlet.ServletContext;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.*;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.util.List;
+import java.util.Map;
 
 
 @RequestScoped
@@ -30,6 +36,8 @@ public class FileEndpoint {
     @Inject
     UsageRepository usageRepository;
 
+    @Context
+    private ServletContext context;
 
 
     @GET
@@ -41,17 +49,68 @@ public class FileEndpoint {
     }
 
     @POST
-    @Path("/uploadfile")
-    @Transactional
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response uploadfile(String name, String path,  @Context UriInfo info, ContentType contentType) {
+    @Path("/upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(MultipartFormDataInput input) throws IOException {
+        Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
+        // Get file data to save
+        List<InputPart> inputParts = uploadForm.get("thumbnail");
+        for (InputPart inputPart : inputParts) {
+            try {
+                MultivaluedMap<String, String> header = inputPart.getHeaders();
+                String fileName = getFileName(header);
+                // convert the uploaded file to inputstream
+                InputStream inputStream = inputPart.getBody(InputStream.class, null);
+                byte[] bytes = IOUtils.toByteArray(inputStream);
 
-        D_File file = new D_File(name,path,contentType);
-        fileRepository.persist(file);
-        return Response.created(URI.create(info.getPath() + "/"+ file.id)).build();
+                /*
+                 * TODO create folder target/mediafiles/video/ or target/mediafiles/video/
+                 *
+                 * following if statement is probably not validating if the mentioned dir exists in the target folder,
+                 * which is mandatory for quarkus?
+                 *
+                 * the media files should also be store at a lfs,
+                 * because the target dir gets cleaned each time quarkus starts
+                 *
+                 * */
+
+                File customDir = new File("mediafiles/video");
+                if (!customDir.exists()) {
+                    customDir.mkdir();
+                }
+                fileName = customDir.getCanonicalPath() + File.separator + fileName;
+                writeFile(bytes, fileName);
+                return Response.status(200).entity("Uploaded file name : " + fileName+" . <br/> <a href='"+context.getContextPath()+"'>Back</a>").build();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 
+    private String getFileName(MultivaluedMap<String, String> header) {
+        String[] contentDisposition = header.getFirst("Content-Disposition").split(";");
+        for (String filename : contentDisposition) {
+            if ((filename.trim().startsWith("filename"))) {
+                String[] name = filename.split("=");
+                String finalFileName = name[1].trim().replaceAll("\"", "");
+                return finalFileName;
+            }
+        }
+        return "unknown";
+    }
+
+    private void writeFile(byte[] content, String filename) throws IOException {
+        System.out.println(filename);
+        File file = new File(filename);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        FileOutputStream fop = new FileOutputStream(file);
+        fop.write(content);
+        fop.flush();
+        fop.close();
+    }
 
     @GET
     @Path("/{id}")
@@ -81,6 +140,4 @@ public class FileEndpoint {
                     .build();
         }
     }
-
-
 }
