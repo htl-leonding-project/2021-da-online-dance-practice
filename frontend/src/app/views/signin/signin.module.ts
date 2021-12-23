981d26d9011a358704b43b171a8c684baf75db00
rosaliemandel <r.mandel@students.htl-leonding.ac.at>
2021-12-23 11:02:12 +0100

    create SigninModule

---
+++ frontend/src/app/views/signin/signin.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SigninComponent } from './signin.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SigninComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    SigninComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatIconModule
+  ]
+})
+export class SigninModule {
+}
