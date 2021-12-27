e4eeeb42a1549949d42d9116175d45d005534ed7
rosaliemandel <r.mandel@students.htl-leonding.ac.at>
2021-12-27 17:08:43 +0100

    handle loggedInState changes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./services/auth.service";
+import {ActivatedRoute, Router, UrlTree} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  constructor(private readonly auth: AuthService,
+              private readonly router: Router,
+              private readonly route: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.auth.loggedInStateAsObservable.subscribe(state => {
+      let redirectUrl: string | UrlTree = '/home';
+
+
+      if (state) {
+        this.route.queryParams.subscribe(value => {
+          if (value["redirectUrl"]) {
+            redirectUrl = value["redirectUrl"];
+          }
+        });
+      } else {
+        this.route.queryParams.subscribe(value => {
+          if (value["redirectUrl"]) {
+            redirectUrl = this.router.createUrlTree(
+              ['/signin'], {
+                queryParams: {
+                  redirectUrl: value["redirectUrl"]
+                }
+              }
+            );
+          } else {
+            redirectUrl = '/signin';
+          }
+        });
+      }
+
+      this.router.navigateByUrl(redirectUrl, {replaceUrl: true})
+        .catch(err => {
+          console.error(err);
+        });
+    });
+  }
 }
