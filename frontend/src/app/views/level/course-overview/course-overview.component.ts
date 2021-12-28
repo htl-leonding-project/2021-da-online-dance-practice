4521d0da7e90de590cc6cdd309349450769c661e
rosaliemandel <r.mandel@students.htl-leonding.ac.at>
2021-12-28 19:29:16 +0100

    add course-overview component

---
+++ frontend/src/app/views/level/course-overview/course-overview.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {BackendService} from "../../../services/backend.service";
+import {Course} from "../../../models/models";
+
+@Component({
+  selector: 'app-course-overview',
+  templateUrl: './course-overview.component.html',
+  styleUrls: ['./course-overview.component.scss']
+})
+export class CourseOverviewComponent implements OnInit {
+  courses: Course[] | null;
+
+  constructor(private readonly route: ActivatedRoute,
+              private readonly backend: BackendService) {
+    this.courses = null;
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(value => {
+      this.backend.get(`course/findByLevel/${value["id"]}`).then(courses => {
+        this.courses = courses as Course[];
+
+      });
+    });
+  }
+
+}
