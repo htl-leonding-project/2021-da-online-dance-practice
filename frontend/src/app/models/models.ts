8bb736ff479d71ad35702aa6d4e5fc837710f3c7
rosaliemandel <r.mandel@students.htl-leonding.ac.at>
2021-12-28 19:30:01 +0100

    add interfaces

--- frontend/src/app/models/models.ts
+++ frontend/src/app/models/models.ts
@@ -1,13 +1,33 @@
 export interface User {
-  firstname: string
-  id: number
-  lastname: string
-  password: string
-  role: Role
+  firstname: string,
+  id: number,
+  lastname: string,
+  password: string,
+  role: Role,
   username: string
 }
 
 export enum Role {
   STUDENT = 'STUDENT',
   TEACHER = 'TEACHER'
+}
+
+export interface Level {
+  id: string,
+  description: string
+}
+
+export interface Course {
+  id: number,
+  title: string,
+  description: string,
+  level: Level
+}
+
+export interface DFile {
+  contentType: string,
+  description: string,
+  id: number,
+  name: string,
+  path: string,
 }
