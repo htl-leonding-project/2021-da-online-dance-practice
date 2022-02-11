412d795bd5e039d987d7d8e4068bd8d1608282fe
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2022-02-11 13:29:51 +0100

    change create endpoint and implemented update endpoint

--- backend/src/main/java/at/htl/boundary/CourseEndpoint.java
+++ backend/src/main/java/at/htl/boundary/CourseEndpoint.java
@@ -1,6 +1,6 @@
+
 package at.htl.boundary;
 
-import at.htl.control.AccessTokenRepository;
 import at.htl.control.CourseRepository;
 import at.htl.control.LevelRepository;
 import at.htl.control.UsageRepository;
@@ -8,10 +8,7 @@ import at.htl.entity.AccessToken;
 import at.htl.entity.Course;
 import at.htl.entity.D_File;
 import at.htl.entity.Level;
-import org.jboss.logging.Logger;
 
-import javax.annotation.security.PermitAll;
-import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -37,74 +34,42 @@ public class CourseEndpoint {
     @Inject
     UsageRepository usageRepository;
 
-    @Inject
-    AccessTokenRepository accessTokenRepository;
-
-    @Inject
-    Logger logger;
-
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @RolesAllowed({"STUDENT", "TEACHER"})
     public Response findAll() {
         return Response.ok(courseRepository.listAll()).build();
     }
 
+    //change 
     @POST
     @Path("/create")
-    @RolesAllowed("TEACHER")
-    public Response create(String levelId, @Context UriInfo info, String title, String description) {
-        Level level = levelRepository.findById(levelId);
-        Course course = new Course(title, description, level);
+    public Response create(@Context UriInfo info, Course course) {
 
         courseRepository.persist(course);
 
         return Response
-                .created(URI.create(info.getPath() + "/" + course.id))
+                .ok(course)
                 .build();
     }
 
     @GET
     @Path("/{id}")
-    @RolesAllowed({"STUDENT", "TEACHER"})
     public Response findById(@PathParam("id") long id) {
         return Response.ok(courseRepository.findById(id)).build();
     }
 
-//    @DELETE
-//    @Path("/{title}")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    @Transactional
-//    public Response delete(@PathParam("title") String title) {
-//
-//        Course course = courseRepository.find("title", title)
-//                .stream()
-//                .findFirst()
-//                .orElse(null);
-//
-//        if (course != null) {
-//            courseRepository.delete("title", course.title);
-//            return Response.ok().build();
-//        } else {
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//       /* try {
-//            courseRepository.delete(title);
-//            return Response
-//                    .noContent()
-//                    .build();
-//        } catch (IllegalArgumentException e) {
-//            return Response
-//                    .status(400)
-//                    .header("Reason", "Course with id" + title + "does not exist")
-//                    .build();
-//        }*/
-//    }
+
+    //change 
+    @PUT
+    @Path("/{id}")
+    public Response update(@PathParam("id") long id, Course course) {
+
+        return Response
+                .ok(courseRepository.save(course)).build();
+    }
 
     @DELETE
     @Path("/{id}")
-    @RolesAllowed("TEACHER")
     public Response delete(@PathParam("id") Long id) {
         try {
             courseRepository.deleteById(id);
@@ -122,7 +87,6 @@ public class CourseEndpoint {
 
     @GET
     @Path("/findByLevel/{levelId}")
-    @RolesAllowed({"STUDENT", "TEACHER"})
     public Response findCourseByLevel(@PathParam("levelId") String levelId) {
         Level level = new Level(levelId.toUpperCase(), levelId.toUpperCase());
         return Response.ok().entity(courseRepository.findCourseByLevel(level)).build();
@@ -130,8 +94,7 @@ public class CourseEndpoint {
 
 
     @GET
-    @Path("/filesByCourse/{courseId}/user")
-    @RolesAllowed({"STUDENT", "TEACHER"})
+    @Path("/filesByCourse/{courseId}")
     public Response findMediaFileByCourse(@PathParam("courseId") long courseId) {
         List<D_File> files = usageRepository.findFilesByCourseId(courseId);
         return Response
@@ -139,32 +102,4 @@ public class CourseEndpoint {
                 .build();
     }
 
-    @GET
-    @Path("/filesByCourse/{courseId}/token")
-    @PermitAll
-    public Response findMediaFileByCourseWithToken(@PathParam("courseId") long courseId, @HeaderParam("X-Token") String token) {
-        AccessToken accessToken = accessTokenRepository.find("token", token)
-                .stream()
-                .findFirst()
-                .orElse(null);
-
-        logger.info(token);
-        if (accessToken != null) {
-            accessToken = accessTokenRepository.activateToken(accessToken);
-
-
-            if (accessTokenRepository.validate(accessToken) && accessToken.course.id == courseId) {
-                List<D_File> files = usageRepository.findFilesByCourseId(courseId);
-                return Response
-                        .ok(files)
-                        .build();
-            }
-        }
-
-        return Response
-                .status(Response.Status.UNAUTHORIZED)
-                .build();
-    }
-
-
-}
+} 
